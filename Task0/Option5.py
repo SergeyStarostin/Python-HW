5d788b2415006374f46cc8be314eea2a8875d45e
Starostin Sergey <Vlillen@yandex.ru>
2023-04-28 01:42:52 +0400

    ЛАБОРАТОРНАЯ РАБОТА №1. Ветвящиеся вычислительные процессы. Вариант 5.

---
+++ Task0/Option5.py
@@ -0,0 +1,22 @@
+'''
+ЛАБОРАТОРНАЯ РАБОТА №1. Ветвящиеся вычислительные процессы
+Вариант №5
+'''
+import math
+
+def result(a, b):
+    if b == 0:
+        print('Деление на 0')
+    elif a < -2:
+        print('Корень из отрицательного числа')
+    elif b < a:
+        print('y1 = ', y := ((a + b) / b ** 2) - a)
+    elif b >= a > 0:
+        print('y2 = ', math.sin(a) + math.cos(b))
+    elif b >= a <= 0:
+        print('y3 = ', math.sqrt(3 * a + b))
+    else:
+        print('Некорректный ввод данных')
+a = int(input('Введите число a: '))
+b = int(input('Введите число b: '))
+result(a, b)
