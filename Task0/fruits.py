7e1ae932291c5fda3fb92b5936e903fe1c491b44
Y127908 <Sergey.starostin@vaz.ru>
2023-05-15 11:29:25 +0400

    one-line program

--- Task0/fruits.py
+++ Task0/fruits.py
@@ -1,17 +1,12 @@
 '''
 1. Создать функцию dict_to_list, которая будет конвертировать словарь в список кортежей;
 2. Функция должна принимать словарь, а возвращать список кортежей, в каждом кортеже
-должны быть парыс(key, value) из словаря;
+должны быть пары(key, value) из словаря;
 3. Если значение ключа - целое число, то его нужно умножить на 2 перед добавлением в кортеж.
 '''
-print(fruits := {'watermelon': 20, 'grape': 40.5, 'apple': 35})
+print(fruits := {'watermelon': 'fresh', 'grape': 40.1, 'apple': 35})
 
-def dict_to_list(fruits):
-    for item in range(len(fruits)):
-        for keys, values in fruits.items():
-            if int(values) == values:
-                print([(keys, values * 2)])
-            elif int(values) != values:
-                print([(keys, values)])
-        return fruits
-dict_to_list(fruits)
+def dict_to_list(dictionary: dict) -> list:
+    return [(keys, (values * 2) if isinstance(values, int) else values) for keys, values in dictionary.items()]
+
+print(dict_to_list(fruits))
